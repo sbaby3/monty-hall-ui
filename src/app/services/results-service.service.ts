10ab5df9886255a96430aede18960f6b22c6ad35
sbaby3 <babyshon92@gmail.com>
2023-09-17 04:19:16 -0400

    updated to use deployed backend api

--- src/app/services/results-service.service.ts
+++ src/app/services/results-service.service.ts
@@ -11,7 +11,8 @@ export class ResultsService {
 
   constructor(private httpClient: HttpClient) { }
 
-  apiUrl = 'http://localhost:8080';
+  // apiUrl = 'http://localhost:8080';
+  apiUrl = 'https://stalwart-way-347600.ue.r.appspot.com';
 
   getResults(totalGames: number): Observable<ResultsResponse> {
     let params = new HttpParams();
